c7f6fc1f2c2d9b2af20c395e8be8ba10c6c06b41
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-01-21 16:27:01 +0900

    post model time setting

--- do_it_django/blog/models.py
+++ do_it_django/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now=True)
+    update_at = models.DateTimeField(auto_now=True)
+    # author : after write
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
