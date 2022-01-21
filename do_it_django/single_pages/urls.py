10350a209843733c3193aef6fb1f8f6ca913e50c
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-01-21 19:58:04 +0900

    make landing page & about me

--- do_it_django/single_pages/urls.py
+++ do_it_django/single_pages/urls.py
@@ -12,12 +12,13 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+    FBV : Function based view 방식으로 구현
 """
-from django.contrib import admin
-from django.urls import path, include
+from . import views
+from django.urls import path
 
 urlpatterns = [
-    path('blog/', include('blog.urls')),
-    path('admin/', admin.site.urls),
-    path('',include('single_pages.urls')),
+    path('about_me/', views.about_me),
+    path('', views.landing),
 ]
