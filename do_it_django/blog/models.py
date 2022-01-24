a5708977554b13cdb4e36e4b3be2591a66acd3d5
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-01-24 16:42:57 +0900

    post_detail in picture

--- do_it_django/blog/models.py
+++ do_it_django/blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/',blank=True)
     created_at = models.DateTimeField(auto_now=True)
     update_at = models.DateTimeField(auto_now=True)
     # author : after write
