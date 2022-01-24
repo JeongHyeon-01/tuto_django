a39547cf53e0a59fc56b89a858d8950cc9aab778
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-01-24 14:50:48 +0900

    CBV make detail_page

--- do_it_django/blog/urls.py
+++ do_it_django/blog/urls.py
@@ -19,6 +19,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path('<int:pk>/',views.PostDetail.as_view()),
     path('',views.PostList.as_view()),
     # path('', views.index),
     # path('<int:pk>/', views.single_post_page),
--- do_it_django/blog/views.py
+++ do_it_django/blog/views.py
@@ -1,13 +1,18 @@
 from re import template
-from django.shortcuts import render
+# from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 # Create your views here.
 
 class PostList(ListView):
     model = Post
     # template_name = 'blog/index.html'
     ordering = '-pk'
+
+
+class PostDetail(DetailView):
+    model = Post
+
 '''
  FBV 로 작성한 문서 (아래)
 '''
