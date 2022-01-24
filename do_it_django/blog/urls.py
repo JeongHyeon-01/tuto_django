7c79d77be36b2129a2182f79f09fe6b21287b16d
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-01-24 14:43:11 +0900

    CBV make blog indexpage

--- do_it_django/blog/urls.py
+++ do_it_django/blog/urls.py
@@ -19,6 +19,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index),
-    path('<int:pk>/', views.single_post_page),
+    path('',views.PostList.as_view()),
+    # path('', views.index),
+    # path('<int:pk>/', views.single_post_page),
 ]
--- do_it_django/blog/views.py
+++ do_it_django/blog/views.py
@@ -1,23 +1,33 @@
+from re import template
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
 
-    return render(
-        request,
-        'blog/index.html',{
-            'posts':posts,
-        }
-    )
+class PostList(ListView):
+    model = Post
+    # template_name = 'blog/index.html'
+    ordering = '-pk'
+'''
+ FBV 로 작성한 문서 (아래)
+'''
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
+#     return render(
+#         request,
+#         'blog/index.html',{
+#             'posts':posts,
+#         }
+#     )
 
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post':post,
-        }
-    )
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post':post,
+#         }
+#     )
